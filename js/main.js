ef3e53bcf6c0b42415a5b15fbacf72fc9ac8a1c4
Samuel Médor <samuel.medor@live.fr>
2023-10-22 08:49:15 +0200

    ajout functionnalité game

--- js/main.js
+++ js/main.js
@@ -1,2 +1,36 @@
-// Logique du jeu sera ajoutée ici
-console.log("Jeu de Memory - Notation Algébrique");
+let currentMove = '';
+let moves = [
+    { notation: 'e4', name: 'Ouverture du pion du roi' },
+    { notation: 'c5', name: 'Défense sicilienne' },
+    { notation: 'd4', name: 'Ouverture de la dame' },
+    { notation: 'e6', name: 'Défense française' }
+];
+
+function randomMove() {
+    return moves[Math.floor(Math.random() * moves.length)];
+}
+
+function displayMove() {
+    let move = randomMove();
+    currentMove = move.name;
+    document.getElementById('notationDisplay').textContent = move.notation;
+}
+
+function checkAnswer(answer) {
+    let feedback = document.getElementById('feedback');
+    if (answer === currentMove) {
+        feedback.textContent = 'Correct!';
+        feedback.style.color = 'green';
+    } else {
+        feedback.textContent = 'Incorrect. La bonne réponse était : ' + currentMove;
+        feedback.style.color = 'red';
+    }
+}
+
+function nextMove() {
+    displayMove();
+    document.getElementById('feedback').textContent = '';
+}
+
+// Initialiser le jeu
+displayMove();
