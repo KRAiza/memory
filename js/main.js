169539a0259f46adbc69af182a501f01161c9546
Samuel Médor <samuel.medor@live.fr>
2023-10-22 09:37:56 +0200

    modification system button

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 let currentMove = '';
 let moves = [
-    { notation: 'e4', name: 'Ouverture du pion du roi' },
-    { notation: 'c5', name: 'Défense sicilienne' },
-    { notation: 'd4', name: 'Ouverture de la dame' },
-    { notation: 'e6', name: 'Défense française' }
+    { 
+        notation: 'e4 e5', 
+        name: 'le pion blanc joue de e2 en e4 et le pion noir joue de e7 en e5' 
+    },
+    { 
+        notation: 'Cf3 Cc6', 
+        name: 'cavalier blanc joue de g1 en f3 et cavalier noir joue de b8 en c6' 
+    },
+    { 
+        notation: 'Fb5 a6', 
+        name: 'fou blanc en b5, pion en a6' 
+    },
+    { 
+        notation: 'Fxc6 dxc6', 
+        name: 'fou prend cavalier et pion d7 prend fou' 
+    }
 ];
 
 function randomMove() {
@@ -11,10 +23,18 @@ function randomMove() {
 }
 
 function displayMove() {
-    let move = randomMove();
-    currentMove = move.name;
-    document.getElementById('notationDisplay').textContent = move.notation;
+  let move = randomMove();
+  currentMove = move.name;
+  document.getElementById('notationDisplay').textContent = move.notation;
+
+  // Mise à jour des boutons
+  let shuffledMoves = shuffleArray(moves);
+  document.getElementById('option1').textContent = shuffledMoves[0].name;
+  document.getElementById('option2').textContent = shuffledMoves[1].name;
+  document.getElementById('option3').textContent = shuffledMoves[2].name;
+  document.getElementById('option4').textContent = shuffledMoves[3].name;
 }
+
 
 function checkAnswer(answer) {
     let feedback = document.getElementById('feedback');
@@ -30,6 +50,14 @@ function checkAnswer(answer) {
 function nextMove() {
     displayMove();
     document.getElementById('feedback').textContent = '';
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 // Initialiser le jeu
